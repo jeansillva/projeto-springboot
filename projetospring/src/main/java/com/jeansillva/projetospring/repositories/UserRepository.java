339b01c59b664d767ccb66af688525e2aec6c18b
Jean <jeancarlos_bh@outlook.com>
2024-02-21 09:45:20 -0300

    Camada de serviço e registro de componentes

--- projetospring/src/main/java/com/jeansillva/projetospring/repositories/UserRepository.java
+++ projetospring/src/main/java/com/jeansillva/projetospring/repositories/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.jeansillva.projetospring.entities.User;
 
+// @Repository opcional nesse caso
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
---
+++ projetospring/src/main/java/com/jeansillva/projetospring/services/UserService.java
@@ -0,0 +1,26 @@
+package com.jeansillva.projetospring.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jeansillva.projetospring.entities.User;
+import com.jeansillva.projetospring.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
